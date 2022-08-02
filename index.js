17465577d42f2e2702abc0230772030975921077
Marquis Willis <marquiswillis99@gmail.com>
2022-08-02 17:52:11 -0400

    included console.table library for output and updated index.js page to include predefined functions and question arrays to run app

--- index.js
+++ index.js
@@ -1,2 +1,66 @@
 const inquirer = require('inquirer');
 const db = require('./config/connection');
+require('console.table');
+
+db.connect(() => {
+    mainMenu();
+});
+
+// array of object literals to hold question arrays depending on main menu selection
+const mainMenuQuestions = [{
+    type: "list",
+    name: "menu",
+    message: "What would you like to do?",
+    choices: ['View all departments', "View all roles",  "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+}];
+
+const addDepartmentQuestions = [{
+
+}];
+
+const addRoleQuestions = [{
+
+}];
+
+const addEmployeeQuestions = [{
+
+}];
+
+const updateEmployeeQuestions = [{
+
+}];
+
+
+
+// main inquirer function that will run so as long as the server is connected
+function mainMenu() {
+
+}
+
+function viewDepartments() {
+
+};
+
+function viewRoles() {
+
+};
+
+function viewEmployees() {
+
+};
+
+function addDepartment() {
+
+};
+
+function addRole() {
+
+};
+
+function addEmployee() {
+    
+};
+
+function updateEmployee() {
+
+};
